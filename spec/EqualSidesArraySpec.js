56612d8c7fb33656db882b5fbcaf789b62f96d64
Tim <TimCPB@github.com>
2020-09-16 23:19:27 +0100

    red test - returns 1 when given a symmetrical array of 3 numbers

--- spec/EqualSidesArraySpec.js
+++ spec/EqualSidesArraySpec.js
@@ -3,4 +3,8 @@ describe("findEvenIndex", function() {
   it("returns 0 when given an array of one number", function() {
     expect(findEvenIndex([2])).toEqual(0)
   })
+
+  it("returns 1 when given a symmetrical array of 3 numbers", function() {
+    expect(findEvenIndex([2, 3, 2])).toEqual(1)
+  })
 })
